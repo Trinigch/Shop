d5567f10336493b43e697d385692768f104dbcef
trinigch <trinigch@gmail.com>
2024-06-05 22:46:19 -0500

    uploading project

---
+++ src/data/productos.js
@@ -0,0 +1,20 @@
+export default {
+	productos: [
+		{
+			id: '1',
+			name: 'Tennis Converse Standard',
+			description: 'Consectetur adipisicing elit.',
+			precio: 500.0,
+			colors: ['black', 'red', 'yellow'],
+			size: ['1,5', '2', '2,5', '3', '4'],
+		},
+		{
+			id: '2',
+			name: 'Tennis Converse 2000',
+			description: 'Consectetur adipisicing elit.',
+			precio: 450.0,
+			colors: ['black', 'red', 'yellow'],
+			size: ['1,5', '2', '2,5', '3', '4'],
+		},
+	],
+};
